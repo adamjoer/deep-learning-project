bd8fde00462e5c37b782f8ca1646e730bfcf5f13
Adam Harald Jørgensen <58829763+adamjoer@users.noreply.github.com>
2025-11-19 16:55:44 +0100

    integrate Accelerate WIP

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+accelerate launch --config_file accelerate_config.yaml train.py
--- train.py
+++ train.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 import torch
 
-# import wandb
+import wandb
 from torch.utils.data import DataLoader
 from torchvision import transforms
 from torchvision.datasets import CIFAR10
@@ -14,7 +14,6 @@ from unet import UNet
 from vdm import VDM
 
 from accelerate import Accelerator
-from accelerate.utils import set_seed
 
 BATCH_SIZE = 128
 TRAIN_NUM_STEPS = 10_000_000
@@ -38,45 +37,37 @@ def training_step(vdm: VDM, x0: torch.Tensor):
     return loss
 
 
-def save_checkpoint(step: int, vdm: VDM, opt: torch.optim.Optimizer, accelerator: Accelerator):
-    checkpoint_file = Path("model.pt")
-    tmp_file = checkpoint_file.with_suffix(f".tmp.{datetime.now().isoformat()}.pt")
-    if checkpoint_file.exists():
-        checkpoint_file.rename(tmp_file)  # Rename old checkpoint to temp file
-    checkpoint = {
-        "step": step,
-        "model": accelerator.get_state_dict(vdm),
-        "opt": opt.state_dict(),
-    }
-    torch.save(checkpoint, checkpoint_file)
-    tmp_file.unlink(missing_ok=True)  # Delete temp file
-
-    # wandb.save(str(checkpoint_file))
+def cycle(dl):
+    # We don't use itertools.cycle because it caches the entire iterator.
+    while True:
+        for data in dl:
+            yield data
 
 
 def main():
     accelerator = Accelerator(split_batches=True)
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device = accelerator.device
     print("Using device:", device)
 
-    # wandb.init(
-    #     project="deep-learning-project",
-    #     config={
-    #         "batch_size": BATCH_SIZE,
-    #         "learning_rate": LR,
-    #         "num_workers": NUM_WORKERS,
-    #         "train_num_steps": TRAIN_NUM_STEPS,
-    #         "save_every": SAVE_EVERY,
-    #         "device": str(device),
-    #     },
-    # )
+    if accelerator.is_main_process:
+        wandb.init(
+            project="deep-learning-project",
+            config={
+                "batch_size": BATCH_SIZE,
+                "learning_rate": LR,
+                "num_workers": NUM_WORKERS,
+                "train_num_steps": TRAIN_NUM_STEPS,
+                "save_every": SAVE_EVERY,
+                "device": str(device),
+            },
+        )
 
     # accelerator = Accelerator(split_batches=True)
     train_set = get_cifar10_dataset(train=True, download=False)
     validation_set = get_cifar10_dataset(train=False, download=False)
 
-    train_dl = DataLoader(
+    training_dataloader = DataLoader(
         train_set,
         batch_size=BATCH_SIZE,
         shuffle=True,
@@ -85,40 +76,65 @@ def main():
     )
 
     unet = UNet()
-    vdm = VDM(unet, image_shape=train_set[0][0].shape, device=device).to(device)
+    vdm = VDM(unet, image_shape=train_set[0][0].shape, device=device)
 
-    opt = accelerator.prepare(torch.optim.AdamW(vdm.parameters(), lr=LR))
+    optimizer = torch.optim.AdamW(vdm.parameters(), lr=LR)
+
+    training_dataloader = training_dataloader
+
     # opt = torch.optim.AdamW(vdm.parameters(), lr=LR)
 
-    # wandb.watch(vdm, log="all", log_freq=100)
+    if accelerator.is_main_process:
+        wandb.watch(vdm, log="all", log_freq=100)
+
+    vdm, optimizer, training_dataloader = accelerator.prepare(vdm, optimizer, training_dataloader)
 
     step = 0
-    with tqdm(initial=step, total=TRAIN_NUM_STEPS) as pbar:
+
+    checkpoint_file = Path("model.pt")
+
+    def save_checkpoint():
+        tmp_file = checkpoint_file.with_suffix(f".tmp.{datetime.now().isoformat()}.pt")
+        if checkpoint_file.exists():
+            checkpoint_file.rename(tmp_file)  # Rename old checkpoint to temp file
+        checkpoint = {
+            "step": step,
+            "model": accelerator.get_state_dict(vdm),
+            "opt": optimizer.state_dict(),
+        }
+        torch.save(checkpoint, checkpoint_file)
+        tmp_file.unlink(missing_ok=True)  # Delete temp file
+
+        wandb.save(str(checkpoint_file))
+
+    train_dl_iter = cycle(training_dataloader)
+    with tqdm(initial=step, total=TRAIN_NUM_STEPS, disable=not accelerator.is_main_process) as pbar:
         while step < TRAIN_NUM_STEPS:
-            (data, label) = next(iter(train_dl))
-            opt.zero_grad()
-            loss = vdm(data.to(device))
-            loss.backward()
-            opt.step()
-            # wandb.log(
-            #     {
-            #         "train/loss": loss.item(),
-            #         "train/step": step,
-            #         "lr": opt.param_groups[0]["lr"],
-            #     },
-            #     step=step,
-            # )
+            (data, label) = next(train_dl_iter)
+            optimizer.zero_grad()
+            loss = vdm(data)
+            accelerator.backward(loss)
+            optimizer.step()
+            wandb.log(
+                {
+                    "train/loss": loss.item(),
+                    "train/step": step,
+                    "lr": optimizer.param_groups[0]["lr"],
+                },
+                step=step,
+            )
 
             pbar.set_description(f"loss: {loss.item():.4f}")
             step += 1
             accelerator.wait_for_everyone()
-            if step % 100 == 0:
-                print(f"[step {step}] loss = {loss.item():.4f}")
-            if step % SAVE_EVERY == 0 and step > 0:
-                save_checkpoint(step, vdm, opt, accelerator)
+            if accelerator.is_main_process:
+                if step % 100 == 0:
+                    print(f"[step {step}] loss = {loss.item():.4f}")
+                if step % SAVE_EVERY == 0 and step > 0:
+                    save_checkpoint()
             pbar.update()
 
-    # wandb.finish()
+    wandb.finish()
 
 
 if __name__ == "__main__":
