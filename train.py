3904d9b5ef5bd87133e1f0104dab26214ae3db16
Adam Harald Jørgensen <58829763+adamjoer@users.noreply.github.com>
2025-11-19 14:39:34 +0100

    Save model every now and then, and add progress bar

--- train.py
+++ train.py
@@ -1,18 +1,22 @@
 # from accelerate import Accelerator
+from datetime import datetime
+from pathlib import Path
+
 import torch
 from torch import Tensor, nn
 from torch.utils.data import DataLoader
 from torchvision import transforms
 from torchvision.datasets import CIFAR10
+from tqdm import tqdm
 
 from unet import UNet
 from vdm import VDM
 
 BATCH_SIZE = 128
 TRAIN_NUM_STEPS = 10_000_000
+SAVE_EVERY = 10_000
 NUM_WORKERS = 4
 LR = 2e-4
-EPOCHS = 1
 
 
 def get_cifar10_dataset(root="data", train=False, download=False):
@@ -28,6 +32,20 @@ def training_step(vdm: VDM, x0: torch.Tensor):
     x0 = x0.to(next(vdm.parameters()).device)
     loss = vdm(x0)
     return loss
+
+
+def save_checkpoint(step: int, vdm: VDM, opt: torch.optim.Optimizer):
+    checkpoint_file = Path("model.pt")
+    tmp_file = checkpoint_file.with_suffix(f".tmp.{datetime.now().isoformat()}.pt")
+    if checkpoint_file.exists():
+        checkpoint_file.rename(tmp_file)  # Rename old checkpoint to temp file
+    checkpoint = {
+        "step": step,
+        "model": vdm.state_dict(),
+        "opt": opt.state_dict(),
+    }
+    torch.save(checkpoint, checkpoint_file)
+    tmp_file.unlink(missing_ok=True)  # Delete temp file
 
 
 def main():
@@ -52,18 +70,21 @@ def main():
 
     opt = torch.optim.AdamW(vdm.parameters(), lr=LR)
 
-    for epoch in range(EPOCHS):
-        for step, (data, label) in enumerate(train_dl):
+    step = 0
+    with tqdm(initial=step, total=TRAIN_NUM_STEPS) as pbar:
+        while step < TRAIN_NUM_STEPS:
+            (data, label) = next(iter(train_dl))
             opt.zero_grad()
             loss = vdm(data.to(device))
             loss.backward()
             opt.step()
-
+            pbar.set_description(f"loss: {loss.item():.4f}")
+            step += 1
             if step % 100 == 0:
                 print(f"[step {step}] loss = {loss.item():.4f}")
-
-            if step == 500:  # stop early for testing
-                break
+            if step % SAVE_EVERY == 0 and step > 0:
+                save_checkpoint(step, vdm, opt)
+            pbar.update()
 
 
 if __name__ == "__main__":
