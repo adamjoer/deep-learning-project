a16bc4be71e2ad704a6afa0dca72a802a621883c
Adam Harald Jørgensen <58829763+adamjoer@users.noreply.github.com>
2025-11-24 16:17:44 +0100

    Add batch job script

---
+++ batch-job.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+### General options
+### -- specify queue --
+BSUB -q hpc
+### -- set the job Name --
+#BSUB -J My_Application
+### -- ask for number of cores (default: 1) --
+BSUB -n 4
+### -- specify that the cores must be on the same host --
+#BSUB -R "span[hosts=1]"
+### -- specify that we need 8GB of memory per core/slot --
+BSUB -R "rusage[mem=8GB]"
+### -- specify that we want the job to get killed if it exceeds 8 GB per core/slot --
+BSUB -M 8GB
+### -- set walltime limit: hh:mm --
+BSUB -W 24:00
+### -- set the email address --
+# please uncomment the following line and put in your e-mail address,
+# if you want to receive e-mail notifications on a non-default address
+BSUB -u s200718@dtu.dk
+### -- send notification at start --
+BSUB -B
+### -- send notification at completion --
+BSUB -N
+### -- Specify the output and error file. %J is the job-id --
+### -- -o and -e mean append, -oo and -eo mean overwrite --
+BSUB -o Output_%J.out
+BSUB -e Output_%J.err
+
+# here follow the commands you want to execute with input.in as the input file
+/zhome/3d/f/151227/miniforge3/envs/deep-learning/bin/accelerate launch --config_file /zhome/3d/f/151227/src/deep-learning-project/accelerate_config.yaml /zhome/3d/f/151227/src/deep-learning-project/train.py
