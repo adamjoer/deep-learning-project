05f6046d51543ca8c47dd9b6b7561a1a63b5d14e
Adam Harald Jørgensen <58829763+adamjoer@users.noreply.github.com>
2025-12-02 21:33:55 +0100

    Increase memory limits in batch job

--- batch-job.sh
+++ batch-job.sh
@@ -8,10 +8,10 @@
 #BSUB -n 4
 ### -- specify that the cores must be on the same host --
 #BSUB -R "span[hosts=1]"
-### -- specify that we need 4GB of memory per core/slot --
-#BSUB -R "rusage[mem=4GB]"
+### -- specify that we need 6GB of memory per core/slot --
+#BSUB -R "rusage[mem=6GB]"
 ### -- specify that we want the job to get killed if it exceeds 5 GB per core/slot --
-#BSUB -M 5GB
+#BSUB -M 7GB
 ### -- set walltime limit: hh:mm --
 #BSUB -W 24:00
 ### -- set the email address --
